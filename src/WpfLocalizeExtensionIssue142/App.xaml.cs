95f1868d0f5fbbdb8ad30eb300c7b324c3f3138a
Otiel <Otiel@users.noreply.github.com>
2019-05-10 17:09:22 +0200

    feat: add source code

---
+++ src/WpfLocalizeExtensionIssue142/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WpfLocalizeExtensionIssue142 {
+
+    public partial class App: Application {
+
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ src/WpfLocalizeExtensionIssue142/UI/Dialogs/WindowMain.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WpfLocalizeExtensionIssue142 {
+
+    public partial class WindowMain: Window {
+
+        public WindowMain() {
+            InitializeComponent();
+        }
+    }
+}
